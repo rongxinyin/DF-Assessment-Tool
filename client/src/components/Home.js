e45b18f26919be68017da59d22b9f1d129b3df4c
Michael Leong <michaelleong1229@gmail.com>
2023-06-29 13:27:30 -0700

    example

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,11 @@
+import Button from '@mui/material/Button';
+
+
 export default function Home() {
   return (
     <div id="Home">
       <h1>Home Page</h1>
+      <Button variant="outlined">Hello World</Button>
     </div>
   );
 }
