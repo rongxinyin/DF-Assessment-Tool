9c708d931dff40d356258339364f117c5a2086b4
Medha M <79490974+MedhaMahanta@users.noreply.github.com>
2023-06-30 14:36:05 -0700

    Database setup (#8)

---
+++ server/models/analytics.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const analyticsSchema = mongoose.Schema(
+  {
+    requestCount: Number,
+  },
+  { collection: "analytics" }
+);
+
+var AnalyticsModel = mongoose.model("Analytics", analyticsSchema);
+
+export default AnalyticsModel;
