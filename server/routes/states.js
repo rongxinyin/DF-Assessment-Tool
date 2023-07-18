643b6202b135ce3c504001a35e2a178a1fedf931
Medha M <79490974+MedhaMahanta@users.noreply.github.com>
2023-07-18 11:44:39 -0700

    Frontend calculations (#11)
    * initial frontend
    
    in the process of adding materialui and formatting  (formatting is very messy right now)
    
    * formatting
    
    formatting first few inputs
    
    * createCaseIDs
    
    * format
    
    ---------
    
    Co-authored-by: clarayin129 <clarayin129@gmail.com>

--- server/routes/states.js
+++ server/routes/states.js
@@ -1,5 +1,9 @@
 import express from "express";
 import StateModel from "../models/states.js";
+import {
+  createCaseIDs,
+  gtaCalculation,
+} from "../../client/src/logic/DR_Calculations.js";
 
 const router = express.Router();
 
@@ -53,6 +57,41 @@ router.get("/:stateName/:fieldName", async (req, res) => {
       res.json(docs[req.params.fieldName]);
     })
     .catch((err) => res.json(err));
+});
+
+//Route to test GTA calculation function (using hardcoded values).
+router.get("/testGTA", async (req, res) => {
+  let caseIDs = [
+    "MAMediumOffice021",
+    "MAMediumOffice022",
+    "MAMediumOffice023",
+    "MAMediumOffice024",
+  ];
+  let CSSB = [
+    {
+      avg_temp: 100,
+      avg_demand: 200,
+    },
+    {
+      avg_temp: 98,
+      avg_demand: 230,
+    },
+    {
+      avg_temp: 87,
+      avg_demand: 240,
+    },
+    {
+      avg_temp: 87,
+      avg_demand: 230,
+    },
+  ];
+  let result = await gtaCalculation("Massachusetts", caseIDs, CSSB);
+  res.json(result);
+});
+
+//Route to test createCaseIDs
+router.get("/testCaseIDs", async (req, res) => {
+  res.json(createCaseIDs("MA", "SmallOffice", 2004, 0, 5));
 });
 
 export default router;
