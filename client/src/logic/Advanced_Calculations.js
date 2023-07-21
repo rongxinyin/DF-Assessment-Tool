88c12f207c2386d604da9bba1824cfe40543b036
Michael Leong <michaelleong1229@gmail.com>
2023-07-20 17:07:31 -0700

    add airproperties

---
+++ client/src/logic/Advanced_Calculations.js
@@ -0,0 +1,52 @@
+
+
+const air_properties = [
+    // Space Temperatures at 50% RH
+    // Air Density (lbm/ft^3)
+    // Enthalpy (btu/lbm)
+    {
+        space_temp: 69,
+        air_density: 0.0747,
+        enthalpy: 24.77
+    },
+    {
+        space_temp: 70,
+        air_density: 0.0746,
+        enthalpy: 25.3
+    },
+    {
+        space_temp: 71,
+        air_density: 0.0744,
+        enthalpy: 25.84
+    },
+    {
+        space_temp: 72,
+        air_density: 0.0743,
+        enthalpy: 26.39
+    },
+    {
+        space_temp: 73,
+        air_density: 0.0741,
+        enthalpy: 26.95
+    },
+    {
+        space_temp: 74,
+        air_density: 0.0739,
+        enthalpy: 27.52
+    },
+    {
+        space_temp: 75,
+        air_density: 0.0738,
+        enthalpy: 28.1
+    },
+    {
+        space_temp: 76,
+        air_density: 0.0736,
+        enthalpy: 28.7
+    },
+    {
+        space_temp: 77,
+        air_density: 0.0735,
+        enthalpy: 29.3
+    },
+]
