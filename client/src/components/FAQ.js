d219d1eda3ee98b065b010c3d66af6636d9c17d0
Tj Garcia <timothyg16@yahoo.com>
2023-07-20 23:20:27 -0700

    Added Border to FAQ

--- client/src/components/FAQ.js
+++ client/src/components/FAQ.js
@@ -39,7 +39,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
         Lorem ipsum dolor sit amet?
         </Typography>
-        <Box bgcolor="secondary.main" p={2}  sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2}  sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main">
           consectetur adipiscing elit.
           </Typography>    
@@ -48,7 +48,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
         How can solar panels help reduce building energy usage?
         </Typography>
-        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main">
           Solar panels can generate clean electricity from sunlight, '
           allowing buildings to offset their energy consumption by using renewable energy, 
@@ -59,7 +59,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
         What is solar energy?
         </Typography>
-        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main">
           Solar energy is a sustainable and renewable form of power derived from the sun's radiation. It is harnessed using photovoltaic cells or solar thermal collectors,
           converting sunlight into electricity or heat to meet various energy needs. As an eco-friendly alternative to fossil fuels, 
@@ -70,7 +70,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
         What is a Packet RTU?
         </Typography>
-        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main">
           An HVAC packaged rooftop unit (RTU) is a self-contained system used for heating and cooling in commercial buildings. It is installed on the roof or ground and contains 
           all major components within a single enclosure, including the compressor, condenser, evaporator, blower, and sometimes a heating element. RTUs are space-saving, energy-efficient, 
@@ -84,7 +84,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
           Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?
         </Typography>
-        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main"> 
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
           </Typography>
@@ -93,7 +93,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
           How can solar power help reduce building energy usage?
         </Typography>
-        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main"> 
           Solar power reduces building energy usage by converting sunlight into electricity, promoting sustainability.
           </Typography>
@@ -102,7 +102,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
           What is the main advantage of using solar power in buildings?
         </Typography>
-        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main"> 
           The main advantage of using solar power in buildings is that it provides a clean and renewable energy source, reducing greenhouse gas emissions and dependence on fossil fuels.
           </Typography>
@@ -111,7 +111,7 @@ export default function FAQ() {
         <Typography variant="h5" color="white.main" sx={{ fontWeight: "bold", m: 1 }} gutterBottom>
         What is the energy duck curve, and why is it important in the context of energy consumption?
         </Typography>
-        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px'}}>
+        <Box bgcolor="secondary.main" p={2} sx={{ borderRadius: '5px', border: "2px solid #F0F0F0",}}>
           <Typography variant="h6" color="white.main"> 
           The energy duck curve represents the fluctuation of energy demand throughout the day, particularly in regions with high solar power capacity. It is important because it 
           highlights the challenges of balancing supply and demand, as it shows a significant drop in demand during peak solar production, followed by a steep increase during the evening 
@@ -129,6 +129,7 @@ export default function FAQ() {
           color="secondary"
           textAlign="center"
           sx={{
+          border: "2px solid #F0F0F0",
           fontWeight: "bold",
           marginTop: 2,
           marginBottom: 3,
