44397b2d82695639418a1502fddf9909ffb45a2a
Tj Garcia <timothyg16@yahoo.com>
2023-07-25 01:40:07 -0700

    Basic Guide

--- client/src/components/UserGuide.js
+++ client/src/components/UserGuide.js
@@ -35,34 +35,31 @@ export default function UserGuide() {
 
   return (
     <ThemeProvider theme={theme} >
-      <Box bgcolor={"primary.main"} p={2} height="100vh" >
+      <Box bgcolor={"primary.main"} p={2} >
         <Grid Container my={1} width="100%" height="100%" direction="column" justifyContent={"flex-start"} alignItems={"flex-start"}> 
 
-          <Grid item sm={4} p={3} textAlign={"center"} >
-          <ButtonGroup
-              variant="contained"
-              aria-label="Disabled elevation buttons"
-              color="secondary"
-              sx={{ marginTop: 1}}
+          <Grid item p={2} textAlign={"center"} mt={2}>
+            <ButtonGroup
+            variant="contained"
+            color="secondary"
             >
-          <Button variant="contained" color="secondary" onClick={() => navigate("/UserGuide")}
-          sx={{width: "200px",height: "50px", marginTop: 1}}>
-          Basic Guide
-          </Button>
-          <Typography variant="h6" color="white.main" textAlign={"center"} sx={{fontWeight: "bold"}}>
-           OR
-          </Typography>
-          <Button variant="contained" color="secondary" onClick={() => navigate("/UserGuide_Advance")}
+            <Button variant="contained" color="secondary" onClick={() => navigate("/UserGuide")}
+            sx={{width: "200px",height: "50px", marginTop: 1, }}>
+            Basic Guide
+            </Button>
+            <Typography variant="h6" color="white.main" textAlign={"center"} sx={{fontWeight: "bold"}}>
+            <pre> OR </pre>
+            </Typography>
+            <Button variant="contained" color="secondary" onClick={() => navigate("/UserGuide_Advance")}
           sx={{width: "200px",height: "50px", marginTop: 1}}>
           Advanced Guide
-          </Button>
-          </ButtonGroup>
+            </Button>
+            </ButtonGroup>
           </Grid>
 
-          <Grid item sm={12} p={3}>
-            <Box bgcolor="secondary.main" p={2}  sx={{ borderRadius: '5px', border: "2px solid #F0F0F0", 
-            }}>
-            <Typography variant="h6" color="white.main" textAlign={"left"} >
+          <Grid item sm={12} p={3} mb={2} >
+            <Box bgcolor="secondary.main" p={2}  sx={{ borderRadius: '5px', border: "2px solid #F0F0F0", }}>
+            <Typography variant="h6" color="white.main" textAlign={"center"} >
             Basic: As the name suggests, this allows you to get up and running with the simpliest 
             inputs to get the Demand Response calculator to work.
             <br></br>
@@ -75,6 +72,59 @@ export default function UserGuide() {
             </Box>
           </Grid>
         </Grid>
+        
+        <hr></hr>
+
+        <Grid Container my={1} width="100%" height="100%" direction="column" justifyContent={"flex-start"} alignItems={"flex-start"}> 
+          <Grid item sm={12} p={3} mt={2}>
+            <Typography variant="h4" color="white.main" textAlign={"Center"} sx={{fontWeight: "bold"}} >
+            Basic Guide
+            </Typography>
+          </Grid>
+          <Grid item sm={12} p={2} >
+            <Typography variant="h5" color="white.main" textAlign={"left"} sx={{fontWeight: "bold"}} >
+              Basic User Inputs
+            </Typography>
+            <Box bgcolor="secondary.main" p={2}  sx={{ borderRadius: '5px', border: "2px solid #F0F0F0", }}>
+            <Typography variant="h6" color="white.main" textAlign={"left"} sx={{fontWeight: "bold"}} >
+              Start by inputing in your building name and building type. Follow that up by adding your buildings floor area and height in square feet (ft²)
+            </Typography>
+            </Box>
+            <Typography mt={3} variant="h5" color="white.main" textAlign={"left"} sx={{fontWeight: "bold"}} >
+              X, Y, & Z
+            </Typography>
+            <Box bgcolor="secondary.main" p={2}  sx={{ borderRadius: '5px', border: "2px solid #F0F0F0", }}>
+            <Typography variant="h6" color="white.main" textAlign={"left"} sx={{fontWeight: "bold"}} >
+              Explanation for input x, y, z
+            </Typography>
+            </Box>
+            <Typography mt={5}variant="h5" color="white.main" textAlign={"Center"} sx={{fontWeight: "bold"}} >
+            Finally, Hit calculate and Analyze your Results!
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container my={2} rowSpacing={1} columnSpacing={1} textAlign={"Left"} >
+        <Grid item sm={12} p={4}> 
+        <Button
+          onClick={() => navigate("/basic")}
+          variant="contained"
+          color="secondary"
+          textAlign="Right"
+          sx={{
+          border: "2px solid #F0F0F0",
+          fontWeight: "bold",
+          marginTop: 2,
+          marginBottom: 2,
+          width: "25%",
+          height: "50px",
+          }}
+          >
+          Basic calculator
+          </Button>
+      </Grid>
+      </Grid>
+
       </Box>
     </ThemeProvider>
   );
