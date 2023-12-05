89754e0c36394a35f558bd614ae7a0e1652affe0
Michael Leong <michaelleong1229@gmail.com>
2023-12-04 22:27:19 -0800

    start database schema

---
+++ server/models/benchmarking.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const benchmarkingSchema = mongoose.Schema(
+    {
+        siteName: { type: String, required: true },
+
+    },
+    { collection: "benchmarking" }
+);
+
+var BenchmarkingModel = mongoose.model("Benchmarking", benchmarkingSchema);
+
+export default BenchmarkingModel;
---
+++ server/routes/benchmarking.js
@@ -0,0 +1,19 @@
+import express from "express";
+import BenchmarkingModel from "../models/benchmarking.js";
+
+const router = express.Router();
+
+
+
+// get all benchmarking data
+// router.get("/:stateName/loadShedDatabase/:case_ID", async (req, res) => {
+//     StateModel.findOne({ stateName: req.params.stateName })
+//       .then((docs) => {
+//         res.json(
+//           docs.loadShedDatabase.find((obj) => obj.case_ID == req.params.case_ID)
+//         );
+//       })
+//       .catch((err) => res.json(err));
+//   });
+
+export default router;
