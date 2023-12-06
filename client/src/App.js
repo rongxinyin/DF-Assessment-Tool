4584a05675ccb9bfc2e54db48b511309f322eed5
Michael Leong <michaelleong1229@gmail.com>
2023-12-06 00:37:12 -0800

    remove userguide.js file

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import Basic from "./components/Basic.js";
 import FAQ from "./components/FAQ.js";
 import Home from "./components/Home.js";
 import NotFound from "./components/NotFound.js";
-import UserGuide from "./components/UserGuide.js";
 
 import AppBar from "./components/SiteAppBar.js";
 
