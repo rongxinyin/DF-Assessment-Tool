58194c3c8ad5d476d1c39bb111083f95f64ded78
Michael Leong <michaelleong1229@gmail.com>
2023-12-05 20:57:15 -0800

    setup schema + working database pushing

--- server/models/benchmarking.js
+++ server/models/benchmarking.js
@@ -2,8 +2,36 @@ import mongoose from "mongoose";
 
 const benchmarkingSchema = mongoose.Schema(
   {
-    siteName: { type: String, required: true },
+    siteID: { type: String, required: true },
+    siteInfo: [
+      {
+        doe_climate_zone: String,
+        city: String,
+        state: String,
+        zip: Number,
+        number_of_floor: Number,
+        total_building_area_ft2: Number,
+        net_selling_area_ft2: Number,
+        total_stock_area_ft2: Number,
+        number_of_HVAC: Number,
+        program: String,
+        utility: String,
+      },
+    ],
+    fieldMetricBaselineRegression: [
+      {
+        event_id: Number,
+        event_date: Date,
+        shed_start_time_date: Date, // date + time
+        shed_end_time_date: Date, // date + time
+        peak_oat: Number,
+        event_avg_oat: Number,
+        peak_demand_intensity_wft2: Number,
+        shed_avg_wft2: Number,
+      },
+    ],
   },
+
   { collection: "benchmarking" }
 );
 
--- server/routes/benchmarking.js
+++ server/routes/benchmarking.js
@@ -14,4 +14,42 @@ const router = express.Router();
 //       .catch((err) => res.json(err));
 //   });
 
+const benchmarking = new BenchmarkingModel({
+  siteID: "ffff",
+  siteInfo: [
+    {
+      doe_climate_zone: "String",
+      city: "String",
+      state: "String",
+      zip: 1,
+      number_of_floor: 1,
+      total_building_area_ft2: 1,
+      net_selling_area_ft2: 1,
+      total_stock_area_ft2: 1,
+      number_of_HVAC: 1,
+      program: "String",
+      utility: "String",
+    },
+  ],
+  fieldMetricBaselineRegression: [
+    {
+      event_id: 1,
+      event_date: new Date("30 July 2010 15:05 GMT-8"),
+      shed_start_time_date: new Date("30 July 2010 14:00 GMT-8"),
+      shed_end_time_date: new Date("30 July 2010 18:00 GMT-8"),
+      peak_oat: 1,
+      event_avg_oat: 1,
+      peak_demand_intensity_wft2: 1,
+      shed_avg_wft2: 1,
+    },
+  ],
+});
+
+benchmarking.save();
+
+router.get("/test", async (req, res) => {
+  res.send(benchmarking);
+  console.log("bruh");
+});
+
 export default router;
