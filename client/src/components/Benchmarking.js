9dbbad522131d7a9716d2eef4c0f1c73e0c4f85b
Michael Leong <michaelleong1229@gmail.com>
2023-12-05 21:38:55 -0800

    change map to icon layer

--- client/src/components/Benchmarking.js
+++ client/src/components/Benchmarking.js
@@ -5,6 +5,7 @@ import { GeoJsonLayer } from "deck.gl";
 import { Map } from "react-map-gl";
 import data from "./testdata.json";
 import mapbox_token from "./mapbox_token.js";
+import { IconLayer } from '@deck.gl/layers';
 
 import { useNavigate } from "react-router-dom";
 
@@ -18,11 +19,12 @@ const MAP_STYLE =
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
+  longitude: -35,
+  latitude: 36.7,
+  zoom: 1.8,
+  maxZoom: 20,
   pitch: 0,
-  bearing: 0,
+  bearing: 0
 };
 
 export default function BenchMarking() {
@@ -34,26 +36,33 @@ export default function BenchMarking() {
     }
   };
 
-  const layers = [
-    new GeoJsonLayer({
-      id: "test",
-      data: data,
-      filled: true,
-      pointRadiusMinPixels: 1,
-      pointRadiusScale: 2000,
-      getPointRadius: (f) => 1,
-      getFillColor: [86, 144, 58, 250],
-      pickable: true,
-      autoHighlight: true,
-      onClick,
-    }),
-  ];
+  const ICON_MAPPING = {
+    marker: { x: 0, y: 0, width: 120, height: 120, mask: true }
+  };
+
+
+  const layers = new IconLayer({
+    id: 'icon-layer',
+    data,
+    pickable: true,
+    // iconAtlas and iconMapping are required
+    // getIcon: return a string
+    iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+    iconMapping: ICON_MAPPING,
+    getIcon: d => 'marker',
+
+    sizeScale: 6,
+    getPosition: d => d.coordinates,
+    getSize: d => 5,
+    getColor: d => [Math.sqrt(d.exits), 140, 0]
+  });
 
   return (
     <DeckGL
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
       layers={layers}
+      getTooltip={({ object }) => object && `${object.name}\n${object.address}`}
     >
       <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
     </DeckGL>
--- server/routes/benchmarking.js
+++ server/routes/benchmarking.js
@@ -3,53 +3,44 @@ import BenchmarkingModel from "../models/benchmarking.js";
 
 const router = express.Router();
 
-// get all benchmarking data
-// router.get("/:stateName/loadShedDatabase/:case_ID", async (req, res) => {
-//     StateModel.findOne({ stateName: req.params.stateName })
-//       .then((docs) => {
-//         res.json(
-//           docs.loadShedDatabase.find((obj) => obj.case_ID == req.params.case_ID)
-//         );
-//       })
-//       .catch((err) => res.json(err));
-//   });
-
+// import test data
 const benchmarking = new BenchmarkingModel({
-  siteID: "ffff",
-  siteInfo: [
-    {
-      doe_climate_zone: "String",
-      city: "String",
-      state: "String",
-      zip: 1,
-      number_of_floor: 1,
-      total_building_area_ft2: 1,
-      net_selling_area_ft2: 1,
-      total_stock_area_ft2: 1,
-      number_of_HVAC: 1,
-      program: "String",
-      utility: "String",
-    },
-  ],
-  fieldMetricBaselineRegression: [
-    {
-      event_id: 1,
-      event_date: new Date("30 July 2010 15:05 GMT-8"),
-      shed_start_time_date: new Date("30 July 2010 14:00 GMT-8"),
-      shed_end_time_date: new Date("30 July 2010 18:00 GMT-8"),
-      peak_oat: 1,
-      event_avg_oat: 1,
-      peak_demand_intensity_wft2: 1,
-      shed_avg_wft2: 1,
-    },
-  ],
+    siteID: "ffff",
+    siteInfo: [
+        {
+            doe_climate_zone: "String",
+            city: "String",
+            state: "String",
+            zip: 1,
+            number_of_floor: 1,
+            total_building_area_ft2: 1,
+            net_selling_area_ft2: 1,
+            total_stock_area_ft2: 1,
+            number_of_HVAC: 1,
+            program: "String",
+            utility: "String",
+        },
+    ],
+    fieldMetricBaselineRegression: [
+        {
+            event_id: 1,
+            event_date: new Date("30 July 2010 15:05 GMT-8"),
+            shed_start_time_date: new Date("30 July 2010 14:00 GMT-8"),
+            shed_end_time_date: new Date("30 July 2010 18:00 GMT-8"),
+            peak_oat: 1,
+            event_avg_oat: 1,
+            peak_demand_intensity_wft2: 1,
+            shed_avg_wft2: 1,
+        },
+    ],
 });
 
 benchmarking.save();
 
-router.get("/test", async (req, res) => {
-  res.send(benchmarking);
-  console.log("bruh");
+
+// add data to database
+router.get("/add", async (req, res) => {
+    res.send(benchmarking);
 });
 
 export default router;
