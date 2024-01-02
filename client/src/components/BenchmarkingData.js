9c647e4b23608448ef24c1bebd98a934e47ec299
leongmichael <michaelleong1229@gmail.com>
2024-01-02 04:35:53 -0800

    implement table data after its been loaded

--- client/src/components/BenchmarkingData.js
+++ client/src/components/BenchmarkingData.js
@@ -13,7 +13,6 @@ import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
@@ -21,10 +20,8 @@ function createData(name, calories, fat, carbs, protein) {
 const rows = [createData("Frozen yoghurt", 159, 6.0, 24, 4.0)];
 
 export default function BenchmarkingData() {
-
   const siteID = useParams(); // get siteID from url
   const [siteData, setSiteData] = useState();
-
 
   const getSiteData = async () => {
     try {
@@ -44,59 +41,81 @@ export default function BenchmarkingData() {
     }
   };
 
-
   useEffect(() => {
     getSiteData().then((data) => {
       setSiteData(data);
     });
   }, []);
 
+  console.log("site data: ", siteData);
 
   return (
     <Box sx={{ margin: 3, marginTop: 5, flexGrow: 1 }}>
       <Typography variant="h4" sx={{ marginBottom: 1 }}>
         Site Info
       </Typography>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Site ID</TableCell>
-              <TableCell align="right">DOE Climate Zone</TableCell>
-              <TableCell align="right">City</TableCell>
-              <TableCell align="right">State</TableCell>
-              <TableCell align="right">Zip</TableCell>
-              <TableCell align="right">Number of Floors</TableCell>
-              <TableCell align="right">Total Building Area (ft²) </TableCell>
-              <TableCell align="right">Net Selling Area (ft²)</TableCell>
-              <TableCell align="right">Total Stock Area (ft²)</TableCell>
-              <TableCell align="right">Number of HVAC</TableCell>
-              <TableCell align="right">Program</TableCell>
-              <TableCell align="right">Utility</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {siteData.siteID}
-              </TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].doe_climate_zone}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].city}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].state}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].zip}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].number_of_floor}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].total_building_area_ft2}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].net_selling_area_ft2}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].total_stock_area_ft2}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].number_of_HVAC}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].program}</TableCell>
-              <TableCell align="right">{siteData.siteInfo[0].utility}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {siteData ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Site ID</TableCell>
+                <TableCell align="right">DOE Climate Zone</TableCell>
+                <TableCell align="right">City</TableCell>
+                <TableCell align="right">State</TableCell>
+                <TableCell align="right">Zip</TableCell>
+                <TableCell align="right">Number of Floors</TableCell>
+                <TableCell align="right">Total Building Area (ft²) </TableCell>
+                <TableCell align="right">Net Selling Area (ft²)</TableCell>
+                <TableCell align="right">Total Stock Area (ft²)</TableCell>
+                <TableCell align="right">Number of HVAC</TableCell>
+                <TableCell align="right">Program</TableCell>
+                <TableCell align="right">Utility</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {siteData.siteID}
+                </TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].doe_climate_zone}
+                </TableCell>
+                <TableCell align="right">{siteData.siteInfo[0].city}</TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].state}
+                </TableCell>
+                <TableCell align="right">{siteData.siteInfo[0].zip}</TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].number_of_floor}
+                </TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].total_building_area_ft2}
+                </TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].net_selling_area_ft2}
+                </TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].total_stock_area_ft2}
+                </TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].number_of_HVAC}
+                </TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].program}
+                </TableCell>
+                <TableCell align="right">
+                  {siteData.siteInfo[0].utility}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Typography variant="body1">Loading...</Typography>
+      )}
     </Box>
   );
 }
