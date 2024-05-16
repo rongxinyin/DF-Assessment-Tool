c836e6c19f667b48263863edd1e920006611d196
Michael Leong <michaelleong1229@gmail.com>
2024-05-15 21:09:09 -0700

    add more sites, output written to single file

--- server/routes/retail_info/retail_site_data_parsing.py
+++ server/routes/retail_info/retail_site_data_parsing.py
@@ -8,82 +8,97 @@ field_metrics_baseline_regression = pd.read_excel('field_metrics_baseline_regres
 
 
 # site_info data parsing
-row_value = site_info.loc[5-2] # get row based on index number, first site starts at index 0 (current row - 2)
 
-# extract the data points from the row
-siteID = row_value['site_id']
-doe_climate_zone = row_value['doe_climate_zone']
-coordinates = row_value['coordinates'].split(',')
-coordinates.reverse()
-city = row_value['city']
-state = row_value['state']
-zip_code = row_value['zip_code']
-number_of_floor = row_value['number_of_floor']
-total_building_area_ft2 = row_value['total_building_area_ft2']
-net_selling_area_ft2 = row_value['net_selling_area_ft2']
-total_stock_area_ft2 = row_value['total_stock_area_ft2']
-number_of_HVAC = row_value['number_of_HVAC']
-program = row_value['Program']
-utility = row_value['utility']
+def getSiteInfo(start, end):
+  directory = "outputs/" + "rows" + str(start) + "-" + str(end) + ".js"
+  f = open(directory, "a")
 
+  for i in range(start, end+1):
+    row_value = site_info.loc[i-2] # get row based on index number, first site starts at index 0 (current row - 2)
 
-# field_metrics_baseline_regression data parsing
-
-out = f"""fieldMetricBaselineRegression: ["""
-# cycle through all datapoints with the siteID from the site_info data
-for index, row in field_metrics_baseline_regression.iterrows():
-    if row['site_id'] == siteID:
-        event_id = row['event_id']
-        event_date = row['event_date'].to_pydatetime().strftime('%d %B %Y')
-        shed_start_time = row['shed_start_time'].to_pydatetime().strftime('%d %B %Y %H:%M %Z') + "GMT-8"
-        shed_end_time = row['shed_end_time'].to_pydatetime().strftime('%d %B %Y %H:%M %Z') + "GMT-8"
-        peak_oat = row['peak_oat']
-        event_avg_oat = row['event_avg_oat']
-        peak_demand_intensity_wft2 = row['peak_demand_intensity_wft2']
-        shed_avg_wft2 = row['shed_avg_wft2']
+    # extract the data points from the row
+    siteID = row_value['site_id']
+    doe_climate_zone = row_value['doe_climate_zone']
+    coordinates = row_value['coordinates'].split(',')
+    coordinates.reverse()
+    city = row_value['city']
+    state = row_value['state']
+    zip_code = row_value['zip_code']
+    number_of_floor = row_value['number_of_floor']
+    total_building_area_ft2 = row_value['total_building_area_ft2']
+    net_selling_area_ft2 = row_value['net_selling_area_ft2']
+    total_stock_area_ft2 = row_value['total_stock_area_ft2']
+    number_of_HVAC = row_value['number_of_HVAC']
+    program = row_value['Program']
+    utility = row_value['utility']
 
 
-        out = out + f"""
-        {{
-          event_id: {event_id},
-          event_date: new Date("{event_date}"),
-          shed_start_time_date: new Date("{shed_start_time}"),
-          shed_end_time_date: new Date("{shed_end_time}"),
-          peak_oat: {peak_oat},
-          event_avg_oat: {event_avg_oat},
-          peak_demand_intensity_wft2: {peak_demand_intensity_wft2},
-          shed_avg_wft2: {shed_avg_wft2},
-        }}, """
+    # field_metrics_baseline_regression data parsing
 
-out = out + f"""
-    ],"""
-        
-
-# formatting output
-output = f"""const site{siteID} = new BenchmarkingModel({{
-  coordinates: [{coordinates[0]}, {coordinates[1]}],
-  siteID: "{siteID}",
-  siteInfo: {{
-    doe_climate_zone: "{doe_climate_zone}",
-    city: "{city}",
-    state: "{state}",
-    zip: {zip_code},
-    number_of_floor: {number_of_floor},
-    total_building_area_ft2: {total_building_area_ft2},
-    net_selling_area_ft2: {net_selling_area_ft2},
-    total_stock_area_ft2: {total_stock_area_ft2},
-    number_of_HVAC: {number_of_HVAC},
-    program: "{program}",
-    utility: "{utility}",
-  }},
-  {out}
-}});"""
+    out = f"""fieldMetricBaselineRegression: ["""
+    # cycle through all datapoints with the siteID from the site_info data
+    for index, row in field_metrics_baseline_regression.iterrows():
+        if row['site_id'] == siteID:
+            event_id = row['event_id']
+            event_date = row['event_date'].to_pydatetime().strftime('%d %B %Y')
+            shed_start_time = row['shed_start_time'].to_pydatetime().strftime('%d %B %Y %H:%M %Z') + "GMT-8"
+            shed_end_time = row['shed_end_time'].to_pydatetime().strftime('%d %B %Y %H:%M %Z') + "GMT-8"
+            peak_oat = row['peak_oat']
+            event_avg_oat = row['event_avg_oat']
+            peak_demand_intensity_wft2 = row['peak_demand_intensity_wft2']
+            shed_avg_wft2 = row['shed_avg_wft2']
 
 
+            out = out + f"""
+            {{
+              event_id: {event_id},
+              event_date: new Date("{event_date}"),
+              shed_start_time_date: new Date("{shed_start_time}"),
+              shed_end_time_date: new Date("{shed_end_time}"),
+              peak_oat: {peak_oat},
+              event_avg_oat: {event_avg_oat},
+              peak_demand_intensity_wft2: {peak_demand_intensity_wft2},
+              shed_avg_wft2: {shed_avg_wft2},
+            }}, """
 
-# TODO: change it so that output is written to a single file with all sites, not a separate file for each site
-directory = "outputs/" + siteID + ".js"
-f = open(directory, "w")
-f.write(output)
-f.close()
-print("Created", directory)
+    out = out + f"""
+        ],"""
+            
+
+    # formatting output
+    output = f"""const site{siteID} = new BenchmarkingModel({{
+      coordinates: [{coordinates[0]}, {coordinates[1]}],
+      siteID: "{siteID}",
+      siteInfo: {{
+        doe_climate_zone: "{doe_climate_zone}",
+        city: "{city}",
+        state: "{state}",
+        zip: {zip_code},
+        number_of_floor: {number_of_floor},
+        total_building_area_ft2: {total_building_area_ft2},
+        net_selling_area_ft2: {net_selling_area_ft2},
+        total_stock_area_ft2: {total_stock_area_ft2},
+        number_of_HVAC: {number_of_HVAC},
+        program: "{program}",
+        utility: "{utility}",
+      }},
+      {out}
+    }}); \n \n"""
+
+
+
+    f.write(output)
+    print("Added site", siteID)
+
+  for i in range(start, end+1):
+    row_value = site_info.loc[i-2] # get row based on index number, first site starts at index 0 (current row - 2)
+
+    # extract the data points from the row
+    siteID = row_value['site_id']
+
+    f.write(f"""await addOrUpdateSite(site{siteID});\n""")
+    
+
+  f.close()
+
+# getSiteInfo(8, 16)
